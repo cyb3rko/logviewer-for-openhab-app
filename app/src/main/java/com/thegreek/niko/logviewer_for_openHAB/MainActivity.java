101f2973f9a6dc1d7afee4431ebb1e0790228089
Cyb3rKo <niko@cyb3rko.de>
2020-06-12 14:16:03 +0200

    automatic update feature added

--- app/src/main/java/com/thegreek/niko/logviewer_for_openHAB/MainActivity.java
+++ app/src/main/java/com/thegreek/niko/logviewer_for_openHAB/MainActivity.java
@@ -1,15 +1,23 @@
 package com.thegreek.niko.logviewer_for_openHAB;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
     static FirebaseAnalytics firebaseAnalytics;
+    private SharedPreferences mySPR;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        SharedPreferences mySPR = this.getSharedPreferences("Speicherstand", 0);
+        mySPR = this.getSharedPreferences("Speicherstand", 0);
+        editor = mySPR.edit();
+        editor.apply();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.start, new MainFragment()).commit();
 
@@ -29,6 +38,44 @@ public class MainActivity extends AppCompatActivity {
         } else {
             firebaseAnalytics.setAnalyticsCollectionEnabled(true);
         }
+
+        updateCheck(this);
+    }
+
+    private void updateCheck(final Activity activity) {
+        AndroidNetworking.get("https://raw.githubusercontent.com/nikothegreek/logviewer-for-openhab-app/master/app/build.gradle")
+                .doNotCacheResponse()
+                .build()
+                .getAsString(new StringRequestListener() {
+                    @Override
+                    public void onResponse(String response) {
+                        String[] parts = response.split("versionCode ");
+                        String[] parts2 = parts[1].split("\n");
+                        int neuesterVersionCode = Integer.parseInt(parts2[0]);
+                        parts = parts2[1].split("\"");
+                        parts2 = parts[1].split("\"");
+                        editor.putString("neuesteVersion", parts2[0]);
+                        editor.apply();
+
+                        if (BuildConfig.VERSION_CODE != neuesterVersionCode) {
+                            System.out.println("----------\nUpdate verfügbar: " + mySPR.getString("neuesteVersion", "") + "\n----------");
+
+                            UpdateDialog updateDialog = new UpdateDialog();
+                            updateDialog.setCancelable(false);
+                            updateDialog.show(getSupportFragmentManager(), "Update-Dialog");
+
+                            ActivityCompat.requestPermissions(activity , new String[]{Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.REQUEST_INSTALL_PACKAGES}, 1);
+                        } else {
+                            System.out.println("----------\nApp auf dem neuesten Stand\n----------");
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        System.out.println("----------\nUpdate-Abfrage fehlgeschlagen: " + anError.getErrorBody() + "\n----------");
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/com/thegreek/niko/logviewer_for_openHAB/UpdateDialog.java
@@ -0,0 +1,78 @@
+package com.thegreek.niko.logviewer_for_openHAB;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Gravity;
+import android.webkit.URLUtil;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.core.content.ContextCompat;
+
+import es.dmoral.toasty.Toasty;
+
+import static android.content.Context.DOWNLOAD_SERVICE;
+
+public class UpdateDialog extends AppCompatDialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final SharedPreferences mySPR = getActivity().getSharedPreferences("Speicherstand", 0);
+        final SharedPreferences.Editor editor = mySPR.edit();
+        editor.apply();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        TextView titleView = new TextView(getContext());
+        titleView.setPadding(32, 32, 32, 32);
+        titleView.setGravity(Gravity.CENTER_HORIZONTAL);
+        titleView.setTypeface(Typeface.DEFAULT_BOLD);
+        titleView.setTextSize(22);
+        titleView.setText(getString(R.string.update_dialog_title));
+
+        TextView messageView = new TextView(getContext());
+        messageView.setPadding(32, 32, 32, 32);
+        messageView.setGravity(Gravity.CENTER_HORIZONTAL);
+        messageView.setTextSize(16);
+        messageView.setText(String.format(getString(R.string.update_dialog_message), mySPR.getString("neuesteVersion", ""), BuildConfig.VERSION_NAME));
+
+        builder.setView(messageView)
+                .setCancelable(false)
+                .setCustomTitle(titleView)
+                .setPositiveButton(getString(R.string.update_dialog_button1), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                            String link = "https://github.com/nikothegreek/logviewer-for-openhab-app/releases/download/v" + mySPR.getString("neuesteVersion", "") + "/LogViewerforopenHAB_" +
+                                    mySPR.getString("neuesteVersion", "") + ".apk";
+                            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(link))
+                                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(link, null, null))
+                                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                            DownloadManager downloadManager = (DownloadManager) getActivity().getSystemService(DOWNLOAD_SERVICE);
+                            downloadManager.enqueue(request);
+                        } else {
+                            Toasty.error(getContext(), getString(R.string.update_dialog_error), Toasty.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton(getString(R.string.update_dialog_button2), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+
+        return builder.create();
+    }
+}
