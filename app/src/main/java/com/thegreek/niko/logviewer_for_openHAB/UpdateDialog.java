b243d2472a587bee7ab32e816eef1bc9a6414c6d
Cyb3rKo <niko@cyb3rko.de>
2020-06-21 23:35:21 +0200

    added access to changelog from the update dialog

--- app/src/main/java/com/thegreek/niko/logviewer_for_openHAB/UpdateDialog.java
+++ app/src/main/java/com/thegreek/niko/logviewer_for_openHAB/UpdateDialog.java
@@ -5,13 +5,19 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DownloadManager;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.Gravity;
+import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.TextView;
 
@@ -45,7 +51,18 @@ public class UpdateDialog extends AppCompatDialogFragment {
         messageView.setPadding(32, 32, 32, 32);
         messageView.setGravity(Gravity.CENTER_HORIZONTAL);
         messageView.setTextSize(16);
-        messageView.setText(String.format(getString(R.string.update_dialog_message), mySPR.getString("neuesteVersion", ""), BuildConfig.VERSION_NAME));
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/nikothegreek/logviewer-for-openhab-app/releases/latest")));
+            }
+        };
+        String message = String.format(getString(R.string.update_dialog_message), mySPR.getString("neuesteVersion", ""), BuildConfig.VERSION_NAME);
+        String index = "changelog";
+        SpannableString spannableString = new SpannableString(message);
+        spannableString.setSpan(clickableSpan, message.indexOf(index), message.indexOf(index) + index.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        messageView.setText(spannableString);
+        messageView.setMovementMethod(LinkMovementMethod.getInstance());
 
         builder.setView(messageView)
                 .setCancelable(false)
