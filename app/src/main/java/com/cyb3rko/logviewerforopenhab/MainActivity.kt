3ab7e5151f71c46285f54c1b7a8365997d27973b
Cyb3rKo <niko@cyb3rko.de>
2020-10-23 11:38:25 +0200

    improved update mechanism

--- app/src/main/java/com/cyb3rko/logviewerforopenhab/MainActivity.kt
+++ app/src/main/java/com/cyb3rko/logviewerforopenhab/MainActivity.kt
@@ -2,18 +2,23 @@ package com.cyb3rko.logviewerforopenhab
 
 import android.Manifest
 import android.app.Activity
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.UnderlineSpan
+import android.util.Log
 import android.view.View
+import android.webkit.URLUtil
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -74,18 +79,16 @@ class MainActivity : AppCompatActivity() {
         editor = mySPR.edit()
         editor.apply()
 
-        if (mySPR.getBoolean("firstStart", true) || mySPR.getString("date", "") == "") {
+//        if (mySPR.getBoolean("firstStart", true)) {
+//        if (mySPR.getBoolean("firstStart", true) || mySPR.getString("date", "") == "") {
 //            val endUserConsent = EndUserConsent(true)
 //            endUserConsent.isCancelable = false
 //            endUserConsent.show(supportFragmentManager, javaClass.name)
 
-        } else {
+//        } else {
             firebaseAnalytics.setAnalyticsCollectionEnabled(true)
-
-            if (!mySPR.getBoolean("tempDisableStart", false)) {
-//                updateCheck(this)
-            }
-        }
+            updateCheck(this)
+//        }
 
         navView.setNavigationItemSelectedListener {
             toolbar.visibility = View.VISIBLE
@@ -137,6 +140,7 @@ class MainActivity : AppCompatActivity() {
                             drawerMenu.findItem(R.id.drawer_end_user_consent).isChecked = false
                         }
                         negativeButton(R.string.end_user_consent_2_button2) {
+                            firebaseAnalytics.setAnalyticsCollectionEnabled(false)
                             editor.putBoolean("firstStart", true).apply()
                             finish()
                             startActivity(Intent(applicationContext, this@MainActivity::class.java))
@@ -169,12 +173,22 @@ class MainActivity : AppCompatActivity() {
                     editor.putString("newestVersion", versionName).apply()
 
                     if (BuildConfig.VERSION_CODE != newestVersionCode) {
-                        val dialogMessage = String.format(getString(R.string.update_dialog_message), mySPR.getString("newestVersion", ""),
-                            BuildConfig.VERSION_NAME)
+                        Log.d(this@MainActivity::class.java.simpleName, "Update available: $versionName")
+                        val dialogMessage = String.format(getString(R.string.update_dialog_message), "$versionName ($versionCode)",
+                            "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})")
+                        val index = dialogMessage.indexOf("changelog")
+                        val spannableString = SpannableString(dialogMessage)
+                        spannableString.setSpan(object : ClickableSpan() {
+                            override fun onClick(p0: View) {
+                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.update_changelog_link))))
+                            }
+                        }, index, index + "changelog".length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
                         MaterialDialog(this@MainActivity).show {
                             title(R.string.update_dialog_title)
-                            message(0, dialogMessage)
+                            message(0, spannableString) {
+                                messageTextView.movementMethod = LinkMovementMethod.getInstance()
+                            }
                             positiveButton(R.string.update_dialog_button_1) {
                                 if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                                     == PackageManager.PERMISSION_GRANTED) {
@@ -183,23 +197,31 @@ class MainActivity : AppCompatActivity() {
                                     Toasty.error(context, getString(R.string.update_dialog_error), Toasty.LENGTH_LONG).show()
                                 }
                             }
-                            negativeButton(R.string.update_dialog_button_2) {
-                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.update_changelog_link))))
-                            }
                         }
 
                         ActivityCompat.requestPermissions(activity, arrayOf(
                             Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.REQUEST_INSTALL_PACKAGES
-                        ), 1
-                        )
+                        ), 1)
                     }
                 }
 
                 override fun onError(anError: ANError) {
                     // nothing to clean up (for PMD)
+                    println("Checking for update failed: $anError")
                 }
             })
+    }
+
+    private fun downloadNewestApk(context: Context, version: String) {
+        val link = String.format(context.getString(R.string.update_download_link), version)
+
+        val request = DownloadManager.Request(Uri.parse(link)).setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            URLUtil.guessFileName(link, null, null))
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadManager.enqueue(request)
     }
 
     override fun onBackPressed() {
